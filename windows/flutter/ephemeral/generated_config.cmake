5de64d780a564bfa582935068ea15fcdeedae85e
VismayaGawriKrishnan <vismayagawrikrishnan@gmail.com>
2025-03-05 22:06:57 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Vismaya\\Downloads\\flutter_windows_3.29.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Vismaya\\Desktop\\x" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Vismaya\\Downloads\\flutter_windows_3.29.0-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Vismaya\\Desktop\\x"
+  "FLUTTER_ROOT=C:\\Users\\Vismaya\\Downloads\\flutter_windows_3.29.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Vismaya\\Desktop\\x\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Vismaya\\Desktop\\x"
+  "FLUTTER_TARGET=C:\\Users\\Vismaya\\Desktop\\x\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Vismaya\\Desktop\\x\\.dart_tool\\package_config.json"
+)
